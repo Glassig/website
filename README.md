246f44bbcafad9600e1bc60257e94a581033601c
Angelina <angelina.v.gegerfelt@gmail.com>
2016-06-25 23:27:58 +0200

    working first bit, began on footer

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # website
 My first attempt at trying to make a website in express js. The goal is that it will become a personal website, like a CV.
+
+ideas:
+clocks. using web-stuff.
+copyright
---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var v_path = __dirname + '/views/';
+var p_path = __dirname+'/public';
+
+app.set('view engine', 'hbs')
+//app.use(helmet());
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(function (req,res,next) {
+	console.log("/" + req.method);
+	next();
+});
+
+app.get("/",function(req,res){
+	res.render('index');
+});
+/*
+app.get("/about",function(req,res){
+	res.sendFile(v_path + "about.html");
+});
+
+app.get("/contact",function(req,res){
+	res.sendFile(v_path + "contact.html");
+});
+
+*/
+app.use("*",function(req,res){
+	res.render('404');
+});
+
+app.listen(3000,function(){
+	console.log("Live at Port 3000");
+});
