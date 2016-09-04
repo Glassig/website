de72fec3989204f65f296dd7302bed6365912556
Angelina <angelina.v.gegerfelt@gmail.com>
2016-09-04 15:58:50 +0200

    script for generating posts creates files that starts with their ids

--- public/scripts/new_post.sh
+++ public/scripts/new_post.sh
@@ -3,12 +3,13 @@
 fname=$1
 ID=$2
 DATE=`date +%Y-%m-%d`
-echo $fname'_'$DATE
-touch ../src/$DATE':'$fname.md
-echo "---">../src/$DATE':'$fname.md
-echo "title:">>../src/$DATE':'$fname.md
-echo "id: "$ID>>../src/$DATE':'$fname.md
-echo "layout: boxes.hbs">>../src/$DATE':'$fname.md
-echo "tags:">>../src/$DATE':'$fname.md
-echo "date: "$DATE"">>../src/$DATE':'$fname.md
-echo "---">>../src/$DATE':'$fname.md
+src_name=../src/$ID':'$DATE':'$fname.md
+echo $src_name
+touch $src_name
+echo "---">$src_name
+echo "title:">>$src_name
+echo "id: "$ID>>$src_name
+echo "layout: boxes.hbs">>$src_name
+echo "tags:">>$src_name
+echo "date: "$DATE"">>$src_name
+echo "---">>$src_name
