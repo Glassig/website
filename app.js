8e274e60d0d345bace3c5fa728b5f0e13c616e3f
Angelina <angelina.v.gegerfelt@gmail.com>
2016-09-04 16:00:25 +0200

    removed work partial, ran metalsmith script with changes to file-name structure and began designing layout for work-section

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 var express = require("express");
+var hbs = require('hbs');
 var app = express();
 var v_path = __dirname + '/views/';
 var p_path = __dirname + '/public/';
-var box_location = p_path + 'build/'
+var post_location = p_path + 'build/';
 var fs = require('fs');
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(p_path));
 app.use(express.static(__dirname+'/css/'));
+app.use(express.static(__dirname+'/node_modules/materialize-css/dist/'))
 
 app.use(function (req,res,next) {
 	console.log("/" + req.method);
 	next();
 });
+
+app.get("/boxes", function(req,res){
+	var all_boxes=[];
+	//var works = [];
+	//var edu = [];
+	fs.readdir(post_location, function(err,files){
+	    if(err) throw err;
+	    files.forEach(function(file){
+	        if(file != '.DS_Store') { // BECAUSE WHY THE FUUUUUUUCK
+		        var content = fs.readFileSync(post_location + file, 'utf8');
+		        all_boxes.push(content);
+		        //console.log(content)
+		    }
+	    });
+	});
+	res.render('index', {box: all_boxes});
+})
 
 app.get("/",function(req,res){
 	res.render('index');
--- public/src/2013-06-10:din_pea_sum.md
+++
@@ -1,9 +0,0 @@
----
-title: Dinnerpeace
-id: work
-layout: boxes.hbs
-tags:
-date: 2013-06-10:2 months
----
-### Summerwork Customer Service
-Work involved handling customer questions and problems, with focus on customer satisfaction.
